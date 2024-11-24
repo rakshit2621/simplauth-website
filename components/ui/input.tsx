3790c9c67911c4abfa4421362792c00a814a848f
Rakshit <rakshit.a.chopade@gmail.com>
2024-11-24 21:21:12 +0530

    2nd push

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -3,10 +3,12 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 //solve this InputProps error
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-// export interface InputProps
-//   extends React.InputHTMLAttributes<HTMLInputElement> {}
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  customProp?: string;
+}
 
-export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+// export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
